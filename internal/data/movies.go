5146f73acbe20ca181a7db11f678e68718185860
r3d5un <r3d5un@pm.me>
2023-07-26 15:56:03 +0200

    Add optimistic concurrency control

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -92,7 +92,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := `
         UPDATE movies
         SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-        WHERE id = $5
+        WHERE id = $5 AND version = $6
         RETURNING version`
 
 	args := []any{
@@ -103,7 +103,17 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.ID,
 	}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
