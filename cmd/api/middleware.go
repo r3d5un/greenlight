edeacac996992c7b9e51898a1b27cd82f3af9d3d
r3d5un <r3d5un@pm.me>
2023-07-30 03:15:58 +0200

    Handle getting the real IP of requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,13 +4,13 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"greenlight.islandwind.me/internal/data"
 	"greenlight.islandwind.me/internal/validator"
@@ -58,11 +58,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
